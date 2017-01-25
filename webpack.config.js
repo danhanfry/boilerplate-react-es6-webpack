6a2405f9f391f8d3e59d28a89c01de8a6761e9c5
Dan Hanfry <Dan Hanfry>
2017-01-25 02:31:50 +0100

    Init

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+var path = require('path')
+var webpack = require('webpack')
+
+
+var stripLogger = 'strip-loader?strip[]=console.error' +
+                              '&strip[]=console.log' +
+                              '&strip[]=console.warn'
+
+
+module.exports = {
+  // The base directory for resolving the entry option
+  context: __dirname,
+
+  entry: {
+    app: 'index.js',
+    vendor: ['babel-polyfill', 'react', 'react-dom']
+  },
+
+  // Various output options, to give us a single bundle.js file with everything resolved and concatenated
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: "{{PKG_NAME}}.js",
+    pathinfo: true
+  },
+
+  // Where to resolve our loaders
+  resolveLoader: {
+    modules: [path.join(__dirname, 'node_modules')],
+    moduleExtensions: ["-loader"],
+  },
+  resolve: {
+    // Directories that contain our modules
+    modules: [path.resolve(__dirname, "lib"), "node_modules"],
+    descriptionFiles: ["package.json"],
+    moduleExtensions: ["-loader"],
+    // Extensions used to resolve modules
+    extensions: ['.js', '.react.js']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['babel', stripLogger],
+        exclude: [/node_modules/]
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({'process.env': {NODE_ENV: '"production"'}}),
+    new webpack.optimize.CommonsChunkPlugin({name: "vendor",
+                                             filename: "vendor.js"}),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false
+      },
+      output: {
+        comments: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+  ],
+
+  // Include mocks for when node.js specific modules may be required
+  node: {
+    fs: 'empty',
+    vm: 'empty',
+    net: 'empty',
+    tls: 'empty'
+  }
+};
